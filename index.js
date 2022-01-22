4a33c385b0c45d4a836d3e06f9579568061e6d9b
andypri <andyprihoda@gmail.com>
2022-01-22 13:43:43 +0100

    Úkol 3: založení

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Javascript 1: 22.01.2022</h1>`;
 appDiv.style.backgroundColor = 'violet';
 
-import './ukol2.js';
+import './ukol3.js';
