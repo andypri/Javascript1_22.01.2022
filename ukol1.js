a9742865c46448d6f9c99bf31e43cc9d04a29eb3
andypri <andyprihoda@gmail.com>
2022-01-22 11:23:48 +0100

    Úkol 1: proměnné a operátory

--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,27 @@
-console.log('Ahoj');
+// console.log('Ahoj');
+
+let jmeno = 'Andrea';
+let vek = 33;
+let vahaVKg = 55;
+let vyskaVMetrech = 1.71;
+
+let bmi = Math.round((vahaVKg / vyskaVMetrech ** 2) * 100) / 100;
+console.log(bmi);
+
+console.log(
+  'Jmenuji se ' +
+    jmeno +
+    ', je mi ' +
+    vek +
+    'let, vážím ' +
+    vahaVKg +
+    'kg, měřím ' +
+    vyskaVMetrech +
+    'm a moje BMI je ' +
+    bmi +
+    '.'
+);
+
+console.log(
+  `Jmenuji se ${jmeno}, je mi ${vek} let, vážím ${vahaVKg} kg, měřím ${vyskaVMetrech}m a moje BMI je ${bmi}.`
+);
