caf55091715c0c5f9113fb630653f841d671ca11
andypri <andyprihoda@gmail.com>
2022-01-22 15:16:48 +0100

    Funkce: s návratovou hodnotu vs. bez návratové hodnoty

--- ukol3.js
+++ ukol3.js
@@ -1,1 +1,15 @@
 console.log('funguju');
+
+// funkce s return:
+function sectiDveCisla(a, b) {
+  return a + b;
+}
+
+console.log(sectiDveCisla(1, 2));
+
+// funkce s console.log
+function odectiDveCisla(a, b) {
+  console.log(a + b);
+}
+
+odectiDveCisla(1, 2);
